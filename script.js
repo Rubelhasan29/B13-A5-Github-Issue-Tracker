d8b9439ba6fd09b620178ac41a7a4af114fef6fd
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-03-06 21:14:20 +0600

    create a function and add a eventListener when we click on the signin button the event will start and we catch the valu of the input through the Dom selectors

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("SignInBtn").addEventListener("click", function(){
+    // get the username input val
+    const usernameInput = document.getElementById("UsernameInput")
+    const username = usernameInput.value;
+    
+    // get the password input val
+    const passwordInput = document.getElementById("passInput")
+    const password = passwordInput.value;
+
+})
