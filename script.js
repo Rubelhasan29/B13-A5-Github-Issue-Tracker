6358940288ebcc194afac361dcea69c0084f5e63
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-03-06 23:11:21 +0600

    Degiened a new page that is home page where i show that a navbar where have a search button where we can search for any purticular issue and in the bottom of the nav make a all buton section and then the all issue count section

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.getElementById("SignInBtn").addEventListener("click", function(){
     // get the username input val
     const usernameInput = document.getElementById("UsernameInput")
@@ -7,5 +6,13 @@ document.getElementById("SignInBtn").addEventListener("click", function(){
     // get the password input val
     const passwordInput = document.getElementById("passInput")
     const password = passwordInput.value;
+
+    //  match them with actual value
+    if(username == "admin" && password == 'admin123'){
+        window.location.assign("/home.html")
+    }else{
+        alert("Wrong Information");
+        return;
+    }
 
 })
